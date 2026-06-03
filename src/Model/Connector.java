e9c59e4d9c6b3a75eb50f2ca1042a87120cf687d
zen <yusyfi.akira@gmail.com>
2026-06-04 06:15:05 +0700

    perbaikan koneksi db

--- src/Model/Connector.java
+++ src/Model/Connector.java
@@ -9,28 +9,32 @@ public class Connector {
      */
     private static String jdbc_driver = "com.mysql.cj.jdbc.Driver";
     private static String nama_db = "db_topup";
-    private static String url_db = "jdbc:mysql://localhost:3306/" + nama_db;
+    private static String url_db = "jdbc:mysql://localhost:3306/db_topup";
     private static String username_db = "root";
     private static String password_db = "";
 
     static Connection conn;
     
-    // Mencoba menghubungkan program kita dengan ke database MySQL.
+    // Mencoba menghubungkan program ke database MySQL.
     public static Connection Connect() {
         try {
-            // 1. Register driver yang akan dipakai
+            // 1. Register driver
             Class.forName(jdbc_driver);
             
-            // 2. Buat koneksi ke database
+            // 2. Buat koneksi
             conn = DriverManager.getConnection(url_db, username_db, password_db);
-
-            // 3. Menampilkan pesan "Connection Success" jika berhasil terhubung ke database.
-            System.out.println("MySQL Connected");
-        } catch (ClassNotFoundException | SQLException exception) {
-            // Menampilkan pesan error ketika MySQL gagal terhubung.
-            System.out.println("Connection Failed: " + exception.getLocalizedMessage());
+            
+            // 3. Pesan sukses
+            System.out.println("MySQL Connected Successfully!");
+            
+        } catch (ClassNotFoundException e) {
+            System.out.println("Driver Tidak Ditemukan! Detail: " + e.getMessage());
+            e.printStackTrace(); // <--- Ini penting untuk melacak error
+        } catch (SQLException e) {
+            System.out.println("Koneksi ke Database Gagal! Detail: " + e.getMessage());
+            e.printStackTrace(); // <--- Ini penting untuk melacak error
         }
         return conn;
     }
-}
+    }
 
